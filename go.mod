8cc795f54afa20965ff7537e6af41b55d8cc1ab9
Philip Silva <philip.silva@protonmail.com>
2021-10-31 20:37:37 +0100

    Update deps, async/generators
    - updated goja-babel
    - cache option
    - translate for-of, async, generators, optionals

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/psilva261/6to5
 
 go 1.16
 
-require github.com/jvatic/goja-babel v0.0.0-20210518112715-d88cf672428a
+require github.com/jvatic/goja-babel v0.0.0-20211030111852-5873af3b41cc
--- main.go
+++ main.go
@@ -1,19 +1,60 @@
 // Conversion of ES6+ into ES5.1 (wip)
 //
-// TODO: turn into a script that uses devjs
+// TODO: turn into a script that uses gojafs
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
 	"github.com/jvatic/goja-babel"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
+var Cache = os.Getenv("ES6TO5_CACHE")
+
+func cached(hs string) (f *os.File, ok bool) {
+	if Cache == "" {
+		return
+	}
+	f, err := os.Open(Cache+"/"+hs)
+	ok = err == nil
+	return
+}
+
+func cache(hs string, bs []byte) {
+	if Cache == "" {
+		return
+	}
+	f, err := os.Create(Cache+"/"+hs)
+	if err != nil {
+		log.Printf("os create: %v", err)
+		return
+	}
+	f.Write(bs)
+	f.Close()
+}
+
 func Main() (err error) {
+	buf := bytes.NewBufferString("")
+	if _, err = io.Copy(buf, os.Stdin); err != nil {
+		return
+	}
+	h := sha256.New()
+	h.Write(buf.Bytes())
+	hs := fmt.Sprintf("%x", h.Sum(nil))
+	if f, ok := cached(hs); ok {
+		defer f.Close()
+		_, err = io.Copy(os.Stdout, f)
+		return
+	}
 	babel.Init(1) // Setup 1 transformer (can be any number > 0)
-	r, err := babel.Transform(os.Stdin, map[string]interface{}{
+	r, err := babel.Transform(
+		strings.NewReader(buf.String()),
+		map[string]interface{}{
 		"plugins": []string{
 			"transform-arrow-functions",
 			"transform-block-scoping",
@@ -21,12 +62,21 @@ func Main() (err error) {
 			"transform-destructuring",
 			"transform-spread",
 			"transform-parameters",
+			"transform-async-to-generator",
+			"transform-regenerator",
+			"transform-for-of",
+			"proposal-optional-chaining",
 		},
 	})
 	if err != nil {
 		return fmt.Errorf("transform: %v", err)
 	}
-	_, err = io.Copy(os.Stdout, r)
+	bs, err := io.ReadAll(r)
+	if err != nil {
+		return fmt.Errorf("read all: %v", err)
+	}
+	cache(hs, bs)
+	fmt.Println(string(bs))
 
 	return
 }
