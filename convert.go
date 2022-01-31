cd13e0e6f87612f1c7e39e85f19fca144aa489bd
Philip Silva <philip.silva@protonmail.com>
2022-01-31 15:47:45 +0100

    Handle also extends

--- convert.go
+++ convert.go
@@ -5,74 +5,132 @@ import (
 	"github.com/tdewolff/parse/v2/js"
 )
 
+/*
+https://medium.com/@dange.laxmikant/simplified-inheritance-in-js-es5-way-60b4ff19b008
+
+function C(a) {
+	this.a=a;
+}
+C.prototype.info=function() {console.log(this.a);}
+function D() {
+	C.call(this, 1); // "super"
+}
+D.prototype=Object.create(C);
+d=new D();
+d.info();
+*/
+
+type supr struct {
+	extends js.IExpr
+}
+
+func (s supr) Enter(n js.INode) js.IVisitor {
+	if v, ok := n.(*js.CallExpr); ok && v.X.String() == "super" {
+		v.X = dotExpr(s.extends.String(), "call")
+		v.Args.List = append([]js.Arg{
+			js.Arg{
+				Value: &js.Var{
+					Data: []byte("this"),
+				},
+			},
+		}, v.Args.List...)
+	}
+	return s
+}
+
+func (s supr) Exit(n js.INode) {}
+
+var ErrAnonClass = errors.New("cannot process anonymous class")
+
 func classToProto(cls *js.ClassDecl, parent *js.Scope) (p js.BlockStmt, err error) {
 	if cls.Name == nil {
-		return p, errors.New("no class name")
+		return p, ErrAnonClass
 	}
 	f := js.FuncDecl{
 		Name: &(*cls.Name),
 	}
 	f.Body.Scope.Parent = parent
-	for _, def := range cls.Definitions {
-		_ = def
+	construct, other := methods(cls)
+	if construct != nil {
+		if cls.Extends != nil {
+			js.Walk(supr{cls.Extends}, construct)
+		}
+		mt := js.FuncDecl{}
+		mt.Body.Scope.Parent = &f.Body.Scope
+		f.Params = construct.Params
+		for _, stmt := range construct.Body.List {
+			f.Body.List = append(f.Body.List, stmt)
+		}
 	}
-	for _, m := range cls.Methods {
+	p.List = append(p.List, f)
+	if cls.Extends != nil {
+		p.List = append(p.List, extend(cls.Name, cls.Extends))
+	}
+	for _, m := range other {
 		name := &js.Var{
 			Data: []byte(m.Name.String()),
 		}
-		mt := js.FuncDecl{
-			Name: name,
-		}
+		mt := js.FuncDecl{}
 		mt.Body.Scope.Parent = &f.Body.Scope
 		for _, stmt := range m.Body.List {
 			mt.Body.List = append(mt.Body.List, stmt)
 		}
-		f.Body.List = append(f.Body.List, mt)
+		mt.Params = m.Params
+		p.List = append(p.List, regMethod(cls.Name, name, mt))
+	}
+	return
+}
+
+func extend(name *js.Var, extends js.IExpr) (s js.ExprStmt) {
+	s.Value = js.BinaryExpr{
+		Op: js.EqToken,
+		X: dotExpr(name.String(), "prototype"),
+		Y: js.CallExpr{
+			X: dotExpr("Object", "create"),
+			Args: js.Args{
+				List: []js.Arg{
+					js.Arg{
+						Value: dotExpr(extends.String(), "prototype"),
+					},
+				},
+			},
+		},
+	}
+	return
+}
+
+func regMethod(clsName, name *js.Var, mt js.FuncDecl) (s js.ExprStmt) {
+	s.Value = js.BinaryExpr{
+		Op: js.EqToken,
+		X:  dotExpr(clsName.String(), "prototype", name.String()),
+		Y:  mt,
+	}
+	return
+}
+
+func dotExpr(items ...string) (expr js.DotExpr) {
+	if len(items) == 2 {
+		expr.X = js.LiteralExpr{
+			Data: []byte(items[0]),
+		}
+
+	} else {
+		expr.X = dotExpr(items[:len(items)-1]...)
+	}
+	expr.Y = js.LiteralExpr{
+		Data: []byte(items[len(items)-1]),
+	}
+	return
+}
+
+func methods(cls *js.ClassDecl) (constructor *js.MethodDecl, other []*js.MethodDecl) {
+	other = make([]*js.MethodDecl, 0, len(cls.Methods))
+	for _, m := range cls.Methods {
 		if m.Name.String() == "constructor" {
-			f.Params = m.Params
-			cl := &js.CallExpr{
-				X: &js.DotExpr{
-					X: &js.Var{
-						Data: []byte("constructor"),
-					},
-					Y: js.LiteralExpr{
-						Data: []byte("call"),
-					},
-				},
-			}
-			cl.Args.List = append(cl.Args.List, js.Arg{Value: &js.Var{Data: []byte("this")}})
-			for _, par := range m.Params.List {
-				arg := js.Arg{
-					Value: par.Default,
-				}
-				_ = arg
-				//cl.Args.List = append(cl.Args.List, arg)
-			}
-			clSt := js.ExprStmt{
-				Value: cl,
-			}
-			f.Body.List = append(f.Body.List, clSt)
-			_ = clSt
+			constructor = m
 		} else {
-			mt.Params = m.Params
-			f.Body.List = append(f.Body.List, &js.ExprStmt{
-				Value: js.BinaryExpr{
-					Op: js.EqToken,
-					X: js.DotExpr{
-						X: &js.Var{
-							Data: []byte("this"),
-						},
-						Y: js.LiteralExpr{
-							Data: []byte(m.Name.String()),
-						},
-					},
-					Y: &js.Var{
-						Data: []byte(m.Name.String()),
-					},
-				},
-			})
+			other = append(other, m)
 		}
 	}
-	p.List = append(p.List, f)
 	return
 }
--- convert_test.go
+++ convert_test.go
@@ -10,13 +10,13 @@ import (
 func TestClassToProto(t *testing.T) {
 	src := `
 	class A {
-		constructor(h, w) {
-			this.height = h;
+		constructor(w, h) {
 			this.width = w;
+			this.height = h;
 		}
 
 		info() {
-			return String(this.height) + ' ' + String(this.width);
+			return String(this.width) + ' ' + String(this.height);
 		}
 	}
 	`
@@ -38,6 +38,52 @@ func TestClassToProto(t *testing.T) {
 	}
 	res := v.Export().(string)
 	if res != "2 5" {
+		t.Fatalf("%v", res)
+	}
+}
+
+func TestClassToProtoExtends(t *testing.T) {
+	src := `
+	class A {
+		constructor(w, h) {
+			this.width = w;
+			this.height = h;
+		}
+
+		info() {
+			return String(this.width) + ' ' + String(this.height);
+		}
+	}
+
+	class B extends A {
+		constructor() {
+			super(10, 20);
+		}
+	}
+	`
+	ast, err := js.Parse(parse.NewInputString(src), js.Options{})
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	clsA := ast.BlockStmt.List[0].(*js.ClassDecl)
+	clsB := ast.BlockStmt.List[1].(*js.ClassDecl)
+	p, err := classToProto(clsA, &ast.BlockStmt.Scope)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	q, err := classToProto(clsB, &ast.BlockStmt.Scope)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	vm := goja.New()
+	src = p.JS() + " " + q.JS() + " b = new B(); b.info();"
+	t.Logf("src=%+v", src)
+	v, err := vm.RunString(src)
+	if err != nil {
+		panic(err)
+	}
+	res := v.Export().(string)
+	if res != "10 20" {
 		t.Fatalf("%v", res)
 	}
 }
